7e4c7cf338aee87dacf6057381aeae66dac0210e
sherobaig <105058026+sherobaig@users.noreply.github.com>
2026-01-15 12:55:38 -0600

    Create boardgame.py

---
+++ Lab01: Board Games/boardgame.py
@@ -0,0 +1,7 @@
+class BoardGame:
+    def __init__(self, gibbons_rating, public_rating, year, min_players, max_playtime):
+        self.g_rating = gibbons_rating
+        self.p_rating = public_rating
+        self.year = year
+        self.min_players = min_players
+        self.max_playtime = max_playtime
