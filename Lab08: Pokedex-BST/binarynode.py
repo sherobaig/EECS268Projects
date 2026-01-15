78f093d6a97a56891da163cfd6bc7ef849ed09ff
sherobaig <105058026+sherobaig@users.noreply.github.com>
2026-01-15 14:41:32 -0600

    Create binarynode.py

---
+++ Lab08: Pokedex-BST/binarynode.py
@@ -0,0 +1,13 @@
+"""Author: Shero Baig
+KUID: 3093709
+Date: April 21, 2023
+Lab: Lab8
+Purpose: Binary node initialization
+"""
+
+class BinaryNode:
+    def __init__(self, entry):
+        # initialization of binary node
+        self.entry = entry
+        self.right = None
+        self.left = None
